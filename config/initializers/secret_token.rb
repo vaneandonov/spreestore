ae7558f85b82cd99b3fcd292b99ba7319139116e
Vane Andonov <vane.andonov@gmail.com>
2013-02-06 09:52:07 +0100

    spree store

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spreestore::Application.config.secret_token = 'f520f826c8264f4c86606b5e6299fd31dfd0d264790b7aada263dc36978c5f954f1108946017bd0f80362fd0af7d2415b72b6ad8ed6c8dc9fc6b28a5d5485292'
